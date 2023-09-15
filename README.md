38197992bd1cac2ffb337f93a333942434633472
anchor <anchorlightforge@gmail.com>
2023-09-15 09:29:50 -0400

    Better readme, basic metadata
    Added basic metadata to the project.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HarmonyMigrationTools
- A library designed to ease the porting process to Flax.
+ **Harmony Migration Tools** is a library of tools and namespaces meant to provide a base layer of compatibility for C# programmers, particularly Unity programmers, looking to port their projects to the Flax Engine.
+
+ The goal is NOT to change the manner in which users interact with Flax to fit their original engine's conventions, but to provide basic support that can be optimized and refactored out of the project as the project adapts to its new engine.  Many functions in this library will be slower than their Unity equivalents and should only be used for testing purposes.
+
+
+ #### Scripting
+
+ This project is still in the early stages of development and contributions are welcome.
+
+ Use the namespace `HarmonyMigrationTools` to access scripting compatibility functions.
+
+ For a quick cheat-sheet on Unity-to-Flax functions as you port your game, see here:
+ https://docs.google.com/spreadsheets/d/1g-8eEEuDxBcz9NEEipNg1gVXity0qfpN3iQbEqhTBKU/edit?usp=sharing 
+
+ #### Assets
+ More to be determined soon.
---
+++ Source/Game/HarmonyMigrationTools/HarmonyMigrationTools.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace HarmonyMigrationTools
+{
+    
+    /// <summary>
+    /// HarmonyMigrationTools Script.
+    /// </summary>
+    public class HarmonyMigrationTools : GamePlugin
+    { 
+        /// <inheritdoc/>
+        public HarmonyMigrationTools()
+        {
+        
+        _description = new PluginDescription
+        {
+            Name = "Harmony Migration Tools",
+            Category = "Other", //todo: fit into categories
+            Description = "",
+            Author = "",
+            IsAlpha=true,
+            IsBeta=false,
+            Version=new Version(0,0,1),
+            RepositoryUrl = "https://github.com/anchorlightforge/HarmonyMigrationTools"
+
+        };
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            // Here you can add code that needs to be called when script is created, just before the first game update
+        }
+        
+        /// <inheritdoc/>
+        public override void Deinitialize()
+        {
+            base.Deinitialize();
+            // Here you can add code that needs to be called when script is enabled (eg. register for events)
+        }
+
+    }
+}
