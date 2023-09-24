886b375c2ad2486afc69dd24d81c2c43e373db2f
Kristijan Trajkovski <kiko_trajkovski@hotmail.com>
2023-09-24 19:55:13 +0200

    Prefab migration setting up basic structure

--- Source/ContentMigratorEditor/PrefabMigrator.cs
+++ Source/ContentMigratorEditor/PrefabMigrator.cs
@@ -1,16 +1,166 @@
 
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Enumeration;
 using System.Linq;
 using System.Threading.Tasks;
 using FlaxEditor;
-using FlaxEditor.Content;
 using FlaxEngine;
+using FlaxEngine.Assertions;
 using YamlDotNet.RepresentationModel;
 
 class PrefabMigrator : AssetMigratorBase
 {
+  // Used for root objects.
+  class UnityBaseDescriptor
+  {
+    public string fileId;
+    public static UnityBaseDescriptor FromYamlNode(YamlNode node)
+    {
+      var keys = (node as YamlMappingNode).Children.Keys.ToArray();
+      Assert.IsTrue(keys.Length == 1);
+      var node0 = node[keys[0]] as YamlMappingNode;
+
+
+      switch ((keys[0] as YamlScalarNode).Value)
+      {
+        case "GameObject":
+          return UnityGameObjectDescriptor.FromYamlNode(node0);
+        case "Transform":
+          return UnityTransformDescriptor.FromYamlNode(node0);
+        case "MonoBehaviour":
+          return MonoBehaviourDescriptor.FromYamlNode(node0);
+      }
+      return new UnityBaseDescriptor();
+    }
+  }
+
+  // Used for inner objects
+  class FileIdDescriptor
+  {
+    public string fileId;
+  }
+
+  class ReferenceDescriptor : FileIdDescriptor
+  {
+    public string guid;
+    public int type;
+  }
+
+  class BitsDescriptor
+  {
+    int bits;
+  }
+  class Vector2Descriptor
+  {
+    public float x;
+    public float y;
+  }
+  class Vector3Descriptor : Vector2Descriptor
+  {
+    public float z;
+  }
+  class Vector4Descriptor : Vector3Descriptor
+  {
+    public float w;
+  }
+  class UnityGameObjectDescriptor : UnityBaseDescriptor
+  {
+    public int staticEditorFlags;
+    public int layer;
+    public List<FileIdDescriptor> components;
+    public static UnityGameObjectDescriptor FromYamlNode(YamlMappingNode node)
+    {
+      return new UnityGameObjectDescriptor();
+    }
+  }
+  class UnityComponentDescriptor : UnityBaseDescriptor
+  {
+    public FileIdDescriptor gameObject;
+    public bool enabled;
+    public void ApplyFromYamlNode(YamlMappingNode node)
+    {
+
+    }
+  }
+  class UnityTransformDescriptor : UnityComponentDescriptor
+  {
+    public FileIdDescriptor father;
+    public List<FileIdDescriptor> children;
+    public Vector3Descriptor localPos;
+    public Vector4Descriptor localRot;
+    public Vector3Descriptor localScale;
+    public static UnityTransformDescriptor FromYamlNode(YamlMappingNode node)
+    {
+      var descriptor = new UnityTransformDescriptor();
+      descriptor.ApplyFromYamlNode(node);
+      return descriptor;
+    }
+  }
+  class ColliderDescriptor : UnityComponentDescriptor
+  {
+    public FileIdDescriptor material;
+    public BitsDescriptor includeLayers;
+    public BitsDescriptor excludeLayers;
+  }
+  class BoxColliderDescriptor : ColliderDescriptor
+  {
+    public Vector3Descriptor size;
+    public Vector3Descriptor center;
+  }
+
+  public enum ScriptParamType
+  {
+    Array,
+    String,
+    Int,
+    Double,
+    ExternalReference,
+    InternalReference,
+    Object
+  }
+
+  class ScriptParamDescriptor
+  {
+    public ScriptParamType ParamType;
+  }
+
+  class ArrayParamDescriptor : ScriptParamDescriptor
+  {
+    public List<ScriptParamDescriptor> InternalParams;
+  }
+
+  class StringParamDescriptor : ScriptParamDescriptor
+  {
+    public string String;
+  }
+
+  class IntParamDescriptor : ScriptParamDescriptor
+  {
+    public int Int;
+  }
+
+  class DoubleParamDescriptor : ScriptParamDescriptor
+  {
+    public double Double;
+  }
+
+  class ReferenceParamDescriptor : ScriptParamDescriptor
+  {
+    public ReferenceDescriptor Reference;
+  }
+
+  class ObjectParamDescriptor : ScriptParamDescriptor
+  {
+    public Dictionary<string, ScriptParamDescriptor> Object;
+  }
+
+  class MonoBehaviourDescriptor : UnityComponentDescriptor
+  {
+    public ReferenceDescriptor script;
+    public Dictionary<string, ScriptParamDescriptor> scriptParams;
+  }
+
   protected override string[] HandledExtensions
   {
     get
@@ -56,7 +206,15 @@ class PrefabMigrator : AssetMigratorBase
       var assetsRelativePath = Path.GetRelativePath(assetsPath, prefabFile);
       var newProjectRelativePath = Path.Join(destinationPath, assetsRelativePath);
 
-      var prefabProxy = Editor.Instance.ContentDatabase.GetProxy<Prefab>();
+
+      var root = new EmptyActor();
+      root.Name = "Root";
+
+      var gameObjects = new Dictionary<string, Actor>();
+      var transforms = new Dictionary<string, Actor>();
+
+      // PrefabManager.CreatePrefab();
+      // var prefabProxy = Editor.Instance.ContentDatabase.GetProxy<Prefab>();
       // TaskCompletionSource<AssetItem> tcs = new TaskCompletionSource<AssetItem>();
       // Action<ContentItem> onContentAdded = (ContentItem contentItem) =>
       // {
@@ -72,7 +230,7 @@ class PrefabMigrator : AssetMigratorBase
       // Editor.Instance.ContentDatabase.ItemAdded -= onContentAdded;
       // var assetItem = Editor.Instance.ContentDatabase.Find(newProjectRelativePath);
 
-      var prefab = FlaxEngine.Content.Load(newProjectRelativePath) as Prefab;
+      // var prefab = FlaxEngine.Content.Load(newProjectRelativePath) as Prefab;
 
       // Iterate docs 
       foreach (var doc in prefabDeserializer.Documents)
@@ -89,7 +247,9 @@ class PrefabMigrator : AssetMigratorBase
           switch (keyStr)
           {
             case "GameObject":
-              Debug.Log("Found GO!");
+              // Create this game object as empty actor
+              var go = new EmptyActor();
+              go.Name = (rootNodeChildren[i].Value["m_Name"] as YamlScalarNode).Value;
               break;
             case "Transform":
               Debug.Log("Found transform!");
